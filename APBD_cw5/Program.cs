990a1444667195adf3ef5d3497e29bd164ebd7ea
s18158 <s18158@pjwstk.edu.pl>
2020-04-19 16:24:29 +0200

    nowy projekt

---
+++ APBD_cw5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBD_cw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
